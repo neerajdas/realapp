0796f1a4cc88f527172d2083a3f46d97f9e528dc
inkgab <keithar123@gmail.com>
2013-12-27 12:14:47 +0530

    redraged routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Realapp::Application.routes.draw do
-#root 'welcome#index'
-root 'comingsoon#index'
+resources :comingsoon
+
+  get "welcome/index"
+root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
